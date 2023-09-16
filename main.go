2f02da1ec13afb12ffee660e2e810ee626290354
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-09-16 15:06:31 +0900

    db接続を関数に分けました

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 	"log"
 )
 
+func connectDatabase() (*gorm.DB, error) {
+	dsn := "root:root@tcp(127.0.0.1:3306)/golang_login_test"
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	// mysql接続
-	dsn := "root:root@tcp(127.0.0.1:3306)/golang_login_test"
-	_, err := gorm.Open(mysql.Open(dsn))
+	_, err := connectDatabase()
 	if err != nil {
 		log.Fatal(err)
 	}
